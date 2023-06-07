5b5578a55edeb87d4a731b871e49aeedb957592b
rodexiy <rodexcontato@gmail.com>
2023-06-06 21:54:17 -0300

    js

--- DOM/Lista de tarefas - 06 - 06 - 2023/js/script.js
+++ DOM/Lista de tarefas - 06 - 06 - 2023/js/script.js
@@ -2,9 +2,10 @@ const botaoAdicionar = document.getElementById("botao-adicionar")
 const listaTarefas = document.querySelector("#lista-tarefas")
 const campoTarefa = document.querySelector("#campo-tarefa")
 
+
 function newUserData() {
     let userData = {
-        tarefas: []
+        tarefas: {}
     }
 
     return userData
@@ -12,32 +13,43 @@ function newUserData() {
 
 let UserData = localStorage.getItem("UserData") ? JSON.parse(localStorage.getItem("UserData")) : newUserData()
 
+console.log(UserData)
 
 function newTarefa(titulo, descricao){
     let tarefa = {
         titulo: titulo,
         descricao: descricao,
+        concluida: "",
     }
 
     return tarefa
 }
 
-function carregarTarefa(titulo, indice, descricao) {
-    
+function setConcluida(titulo, value) {
+    UserData.tarefas[titulo].concluida = value
+    console.log(UserData.tarefas[titulo])
 }
 
-function criarTarefa(_titulo, indice, descricao) {
-    const titulo = _titulo ? campoTarefa.value : _titulo
+function adicionarTarefa() {
+    const titulo = campoTarefa.value
+    let descricao = ""
+    console.log(titulo)
 
-    if (titulo.length == 0) {
-        return console.log("O campo não pode ser vazio!")
-    }
+    let novaTarefa = newTarefa(titulo, descricao)
+    UserData.tarefas[titulo] = novaTarefa
+
+    carregarTarefa(titulo, descricao)
+}
+
+function carregarTarefa(tarefa) {
+    var titulo = tarefa.titulo ? tarefa.titulo: campoTarefa.value
+
+    if (titulo.length == 0) {return}
 
     let elemento = document.createElement('li')
     elemento.className = "tarefa"
     elemento.textContent = titulo
     listaTarefas.appendChild(elemento)
-
 
     let botaoRemover = document.createElement('a')
     botaoRemover.textContent = "Remover",
@@ -45,32 +57,55 @@ function criarTarefa(_titulo, indice, descricao) {
 
     elemento.appendChild(botaoRemover)
 
+    if (UserData.tarefas[titulo].concluida == true) {
+        elemento.classList.toggle('concluida')
+        botaoRemover.classList.toggle("concluida_remover")
+    }
+
+    elemento.onclick = function() {
+        if (!UserData.tarefas[titulo]) {return}
+        elemento.classList.toggle('concluida')
+        botaoRemover.classList.toggle("concluida_remover")
+    
+        if (UserData.tarefas[titulo].concluida != true) {
+            setConcluida(titulo, true)
+        }else{
+            setConcluida(titulo, false)
+        }
+    }
+
+
     botaoRemover.onmouseover = function(){
-        elemento.style.borderColor = "#BE171C"
-        botaoRemover.style.backgroundColor  = "#BE171C"
-        botaoRemover.style.borderColor  = "#BE171C"
+        if (UserData.tarefas[titulo].concluida) {return }
+
     }
 
     botaoRemover.onmouseout = function(){
+        if (UserData.tarefas[titulo].concluida) {return }
         elemento.style.borderColor = "#5f61ff"
         botaoRemover.style.backgroundColor  = "#5f61ff"
         botaoRemover.style.borderColor  = "#5f61ff"
     }
     
     botaoRemover.onclick = function(){
+        UserData.tarefas[titulo] = undefined
         elemento.remove()
-        UserData.tarefas.splice(indice, 1)
     }
-
-
-    
 }
 
-botaoAdicionar.addEventListener("click", criarTarefa)
+botaoAdicionar.addEventListener("click", adicionarTarefa)
+campoTarefa.addEventListener("keyup", function(enter) {
+    if (enter.key != "Enter") {return}
 
-UserData.tarefas.forEach(tarefa => {
-    criarTarefa(tarefa.titulo)
-});
+    adicionarTarefa()
+})
+
+for (const [nome, obj] of Object.entries(UserData.tarefas)) {
+    console.log(nome, obj)
+    carregarTarefa(obj)
+}
+
+
 
 window.addEventListener('beforeunload', function (e) {
     localStorage.setItem("UserData", JSON.stringify(UserData))
